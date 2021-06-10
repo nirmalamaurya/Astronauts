6f652c142603013a0929efa000be12e5524a0564
nirmala <nirmala.maurya@capgemini.com>
2021-06-10 19:36:31 +1000

    UI design for Astronaut list display

--- SuncorpAstronauts/SuncorpAstronauts/ViewController.swift
+++ SuncorpAstronauts/SuncorpAstronauts/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView : UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+}
+extension ViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let astronautCell : AstronautCell = tableView.dequeueReusableCell(withIdentifier: "astronautCell", for: indexPath) as? AstronautCell{
+                 return astronautCell
+        }
+        return UITableViewCell()
+    }
+    
+    
+}
+extension ViewController : UITableViewDelegate{
+    
 }
 
